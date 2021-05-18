aa225a0aa96e3f27201af2c30a8817c76f529f87
Sovietball1.0 <77866829+Sovietball1922@users.noreply.github.com>
2021-05-19 01:31:01 +0900

    neru

--- LabNote/Form1.cs
+++ LabNote/Form1.cs
@@ -50,32 +50,156 @@ namespace LabNote
         private void ToggleButtons_CheckedChanged(object sender, EventArgs e)
         {
             ToolStripButton button = sender as ToolStripButton;
-            if (button.Checked == false)
+            Font baseFont = richTextBox1.SelectionFont;
+            float offsetSize = (float)1.5;
+            int offsetLocation = 2;
+            if (button.Checked == true)
             {
                 switch (button.Name)
                 {
                     case "toolStripButton2":
-                        Font baseFont = richTextBox1.SelectionFont;
-                        Font fnt = new Font(baseFont.FontFamily,
+                        Font fnt0 = new Font(baseFont.FontFamily,
                                             baseFont.Size,
                                             baseFont.Style | FontStyle.Italic);
-                        richTextBox1.SelectionFont = fnt;
+                        richTextBox1.SelectionFont = fnt0;
 
                         baseFont.Dispose();
-                        fnt.Dispose();
+                        fnt0.Dispose();
                         break;
                     case "toolStripButton3":
+                        Font fnt1 = new Font(baseFont.FontFamily,
+                                            baseFont.Size,
+                                            baseFont.Style | FontStyle.Underline);
+                        richTextBox1.SelectionFont = fnt1;
+
+                        baseFont.Dispose();
+                        fnt1.Dispose();
                         break;
                     case "toolStripButton4":
+                        Font fnt2 = new Font(baseFont.FontFamily,
+                                            baseFont.Size,
+                                            baseFont.Style | FontStyle.Bold);
+                        richTextBox1.SelectionFont = fnt2;
+
+                        baseFont.Dispose();
+                        fnt2.Dispose();
                         break;
                     case "toolStripButton5":
+                        Font fnt3 = new Font(baseFont.FontFamily,
+                                            baseFont.Size,
+                                            baseFont.Style | FontStyle.Strikeout);
+                        richTextBox1.SelectionFont = fnt3;
+
+                        baseFont.Dispose();
+                        fnt3.Dispose();
                         break;
                     case "toolStripButton6":
+                        if (toolStripButton7.Checked == true)
+                        {
+                            toolStripButton7.Checked = false;
+                            Font fnt40 = new Font(baseFont.FontFamily,
+                                                 baseFont.Size + offsetSize,
+                                                 baseFont.Style);
+                            richTextBox1.SelectionFont = fnt40;
+                            richTextBox1.SelectionCharOffset = 0;
+
+                            baseFont.Dispose();
+                            fnt40.Dispose();
+                        }
+                        Font fnt4 = new Font(baseFont.FontFamily,
+                                             baseFont.Size - offsetSize,
+                                             baseFont.Style);
+                        richTextBox1.SelectionFont = fnt4;
+                        richTextBox1.SelectionCharOffset = offsetLocation;
+
+                        baseFont.Dispose();
+                        fnt4.Dispose();
                         break;
                     case "toolStripButton7":
+                        if (toolStripButton6.Checked == true)
+                        {
+                            toolStripButton6.Checked = false;
+                            Font fnt50 = new Font(baseFont.FontFamily,
+                                                  baseFont.Size + offsetSize,
+                                                  baseFont.Style);
+                            richTextBox1.SelectionFont = fnt50;
+                            richTextBox1.SelectionCharOffset = 0;
+
+                            baseFont.Dispose();
+                            fnt50.Dispose();
+                        };
+                        Font fnt5 = new Font(baseFont.FontFamily,
+                                             baseFont.Size - offsetSize,
+                                             baseFont.Style);
+                        richTextBox1.SelectionFont = fnt5;
+                        richTextBox1.SelectionCharOffset = -offsetLocation;
+
+                        baseFont.Dispose();
+                        fnt5.Dispose();
                         break;
                 }
-                button.Checked = true;
+            }
+            else if (button.Checked == false)
+            {
+                switch (button.Name)
+                {
+                    case "toolStripButton2":
+                        Font fnt0 = new Font(baseFont.FontFamily,
+                                            baseFont.Size,
+                                            baseFont.Style & ~FontStyle.Italic);
+                        richTextBox1.SelectionFont = fnt0;
+
+                        baseFont.Dispose();
+                        fnt0.Dispose();
+                        break;
+                    case "toolStripButton3":
+                        Font fnt1 = new Font(baseFont.FontFamily,
+                                            baseFont.Size,
+                                            baseFont.Style & ~FontStyle.Underline);
+                        richTextBox1.SelectionFont = fnt1;
+
+                        baseFont.Dispose();
+                        fnt1.Dispose();
+                        break;
+                    case "toolStripButton4":
+                        Font fnt2 = new Font(baseFont.FontFamily,
+                                            baseFont.Size,
+                                            baseFont.Style & ~FontStyle.Bold);
+                        richTextBox1.SelectionFont = fnt2;
+
+                        baseFont.Dispose();
+                        fnt2.Dispose();
+                        break;
+                    case "toolStripButton5":
+                        Font fnt3 = new Font(baseFont.FontFamily,
+                                            baseFont.Size,
+                                            baseFont.Style & ~FontStyle.Strikeout);
+                        richTextBox1.SelectionFont = fnt3;
+
+                        baseFont.Dispose();
+                        fnt3.Dispose();
+                        break;
+                    case "toolStripButton6":
+                        Font fnt4 = new Font(baseFont.FontFamily,
+                                              baseFont.Size + offsetSize,
+                                              baseFont.Style);
+                        richTextBox1.SelectionFont = fnt4;
+                        richTextBox1.SelectionCharOffset = 0;
+
+                        baseFont.Dispose();
+                        fnt4.Dispose();
+                        break;
+                    case "toolStripButton7":
+                        Font fnt5 = new Font(baseFont.FontFamily,
+                                             baseFont.Size + offsetSize,
+                                             baseFont.Style);
+                        richTextBox1.SelectionFont = fnt5;
+                        richTextBox1.SelectionCharOffset = 0;
+
+                        baseFont.Dispose();
+                        fnt5.Dispose();
+                        break;
+                }
             }
         }
     }
